f2eabf08dbaddd90d547a0bc467e3dc018299873
abdiangel <abdiangel.30@gmail.com>
2018-02-10 00:12:44 -0400

    Modal restructured and color picker added

--- settings_page.php
+++ settings_page.php
@@ -1,12 +1,35 @@
 <?php
-class settings_page{    
+class settings_page{
+    
+    private static $instance = null;
+    
+    public $options;
+    
+    public static function get_instance() {
+  
+        if ( null == self::$instance ) {
+            self::$instance = new self;
+        }
+  
+        return self::$instance;
+  
+    }    
 
     public function __construct(){
         // Add the options page and menu item
         add_action( 'admin_menu', array( $this, 'tmssg_plugin_menu' ));
         
         // Adds all of the options for the administrative settings
-        //add_action( 'admin_init', array( $this, 'plugin_options_init' ));
+        add_action( 'admin_init', array( $this, 'tmssg_options_init' ));
+
+        // Color Picker
+        wp_enqueue_style( 'wp-color-picker' );
+
+        // Register javascript
+        add_action('admin_enqueue_scripts', array( $this, 'enqueue_admin_js' ) );
+
+        // Get registered option
+        $this->options = get_option( 'tmssg_settings_options' );
     }
 
     public function tmssg_plugin_menu() {
@@ -22,10 +45,99 @@ class settings_page{
     public function tmssg_plugin_settings() {
         if ( !current_user_can( 'administrator' ) )  {
             wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
-        }
-        echo '<div class="wrap">';
-        echo '<p>Here is where the form would go if I actually had options.</p>';
-        echo '</div>';
+        }?>
+        <div class="wrap">
+     
+        <h2>Track Message</h2>
+        <form method="post" action="options.php">     
+        <?php 
+            settings_fields(__FILE__);      
+            do_settings_sections(__FILE__);
+            submit_button();
+        ?>
+        </form>
+    </div> <!-- /wrap -->
+    <?php  
+    }
+    public function tmssg_options_init() { 
+     
+        // Add Section for option fields
+        add_settings_section( 'tmssg_section', 'Track Message Options', array( $this, 'displaySection' ), __FILE__ ); // id, title, display cb, page
+         
+        // Add Title Field
+        add_settings_field( 'tmssg_title_field', 'Track Message Title', array( $this, 'titleSettingsField' ), __FILE__, 'tmssg_section' ); // id, title, display cb, page, section
+         
+        // Add Background Color Field
+        add_settings_field( 'tmssg_bg_field', 'Background Color', array( $this, 'bgSettingsField' ), __FILE__, 'tmssg_section' ); // id, title, display cb, page, section
+         
+        // Register Settings
+        register_setting( __FILE__, 'tmssg_settings_options', array( $this, 'validateOptions' ) ); // option group, option name, sanitize cb 
+    }
+
+    // Functions that display fields in settings page
+    public function titleSettingsField() { 
+     
+        $val = ( isset( $this->options['title'] ) ) ? $this->options['title'] : '';
+        echo '<input type="text" name="tmssg_settings_options[title]" value="' . $val . '" />';
+    }   
+     
+    public function bgSettingsField() { 
+         
+        $val = ( isset( $this->options['title'] ) ) ? $this->options['background'] : '';
+        echo '<input type="text" name="tmssg_settings_options[background]" value="' . $val . '" class="tmssg-color-picker" >';
+         
+    }
+
+    
+    // Function that will validate all fields.
+
+    public function validateOptions( $fields ) { 
+     
+    $valid_fields = array();
+     
+    // Validate Title Field
+    $title = trim( $fields['title'] );
+    $valid_fields['title'] = strip_tags( stripslashes( $title ) );
+     
+    // Validate Background Color
+    $background = trim( $fields['background'] );
+    $background = strip_tags( stripslashes( $background ) );
+     
+    // Check if is a valid hex color
+    if( FALSE === $this->checkColor( $background ) ) {
+     
+        // Set the error message
+        add_settings_error( 'tmssg_settings_options', 'tmssg_bg_error', 'Insert a valid color for Background', 'error' ); // $setting, $code, $message, $type
+         
+        // Get the previous valid value
+        $valid_fields['background'] = $this->options['background'];
+     
+    } else {
+     
+        $valid_fields['background'] = $background;  
+     
+    }
+     
+    return apply_filters( 'validateOptions', $valid_fields, $fields);
+}
+ 
+
+    // Function that will check if value is a valid HEX color.
+    public function checkColor( $value ) { 
+     
+    if ( preg_match( '/^#[a-f0-9]{6}$/i', $value ) ) { // If the user insert a HEX color with #     
+        return true;
+    }
+     
+    return false;
+    }
+
+    public function displaySection() { /* Leave blank */ } 
+     
+    //Function that will add javascript file for Color Piker.
+    public function enqueue_admin_js() { 
+     
+    // Add the wp-color-picker dependecy to js file
+    wp_enqueue_script( 'tmssg_custom_js', plugins_url( 'jquery.custom.js', __FILE__ ), array( 'jquery', 'wp-color-picker' ), '', true  );
     }
 }   
-?>
--- track_message.php
+++ track_message.php
@@ -22,36 +22,38 @@ if (is_admin()) {
   }
 
 //CSS & JS
-add_action( 'wp_enqueue_scripts', 'my_scripts' );
+add_action( 'wp_enqueue_scripts', 'myScripts' );
 
 
-function my_scripts(){
+function myScripts(){
     $url_plugin_js  =   plugins_url('track-message/js/');
     $url_plugin_css  =   plugins_url('track-message/css/');
 
     wp_register_script('tmssg_js', $url_plugin_js . 'track_message.js');   
     wp_register_style( 'tmssg_css', $url_plugin_css . 'track_message.css');
-    wp_register_style('prefix_bootstrap','//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css');
     wp_enqueue_style('tmssg_css');
     wp_enqueue_script('tmssg_js');
-    wp_enqueue_style('prefix_bootstrap');
 }
 
 
 class TrackMessage{
-    public static function tmssg_show_message(){
-        $message = 'Estás siendo rastreado si estás de acuerdo con esto prosigue';
-        echo "<div class='track-message text-center backdrop ModalOpen' id='modal'>
-        <div class='container'>
-          <h2 class='text-danger'>WARNING!</h2>
-          <img src='http://res.cloudinary.com/abdiangel/image/upload/c_scale,w_305/v1517783515/12414868-stop-sign-illustration_ypc4rm.jpg' class='img-fluid'>
-          <p class='lead'>$message</p>
-          <button type='button' class='button' id='close'> Estoy de acuerdo con esto</button>
-        </div>
-      </div>>";
-    }
-
+  public static function tmssgShowMessage(){
+    $html= sprintf("<div id='Modal__TrackMessageModal_Id--3455' class='TrackMessageModal'>");
+    $html.= sprintf("<div class='TrackMessageModal__content TrackMessageModal__content--openmodal'>");
+    $html.= sprintf("<div class='TrackMessageModal__content--header'>");
+    $html.= sprintf("<h2>AVISO</h2>");
+    $html.= sprintf("</div>");
+    $html.= sprintf("<div class='TrackMessageModal__content--body'>");
+    $html.= sprintf("<p>Nuestro sitio usa cookies para mejorar la experiencia, si continúa navegando, acepta nuestra políticas</p>");
+    $html.= sprintf("</div>");
+    $html.= sprintf("<div class='TrackMessageModal__content--footer'>");
+    $html.= sprintf("<button type='button' class='TrackMessageModal__content--footer-close' id='Modal_TrackMessageModal_Id--close-5644'> Estoy de acuerdo con esto</button>");
+    $html.= sprintf("</div>");
+    $html.= sprintf("</div>");
+    $html.= sprintf("</div>");
+    echo $html;
+  }
 }
 
-add_action('wp_head', array('TrackMessage', 'tmssg_show_message'));
+add_action('wp_head', array('TrackMessage', 'tmssgShowMessage'));
 
